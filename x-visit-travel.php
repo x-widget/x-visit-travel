828d0a69bf5987542bd295d52b9c8dd156d101ea
Arvin Domingo <arvin.o.domingo@gmail.com>
2014-04-25 21:13:35 +0800

    display 0 if num of visits if empty

--- x-visit-travel.php
+++ x-visit-travel.php
@@ -10,8 +10,11 @@ $visit_name = array('오늘', '어제', '최대', '전체');
 		<img src="<?=x::url()?>./widget/<?=$widget_config['name']?>/img/stat-icon.png"/>접속자 통계
 	</div>
 	<div class='stats-content'>
-		<?for( $i=0; $i<=3; $i++) { ?>
-			<div class='per-stats'><?=$visit_name[$i]?> <span class='stats-results'><?=number_format($visit[$i+1])?></span></div>
+		<?for( $i=0; $i<=3; $i++) { 
+			if( $visit[$i+1] ) $num_of_visits = number_format($visit[$i+1]);
+			else $num_of_visits = 0;			
+		?>
+			<div class='per-stats'><?=$visit_name[$i]?> <span class='stats-results'><?=$num_of_visits?></span></div>
 		<?}?>
 		<?php if ($is_admin == "super") {  ?><a href="<?php echo G5_ADMIN_URL ?>/visit_list.php">상세보기</a><?php } ?>
 	</div>
